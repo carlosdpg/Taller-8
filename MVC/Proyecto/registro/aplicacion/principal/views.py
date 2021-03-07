3b6a1a27129c4753482117c1555a5fed500aadd8
Carlos Daniel Parra <cdanielpg@outlook.com>
2021-03-07 13:36:59 -0500

    CreacionTemplates
    Y otras cosas

--- MVC/Proyecto/registro/aplicacion/principal/views.py
+++ MVC/Proyecto/registro/aplicacion/principal/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Persona
+from .models import TipoDocumento
+from .models import Ciudad
 
 # Create your views here.
+
+def inicio(request):
+    return render(request,'index.html')
