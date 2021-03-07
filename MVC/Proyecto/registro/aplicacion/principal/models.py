b2bca5f21194ffa20f7de94718336b0b78a9bf99
Carlos Daniel Parra <cdanielpg@outlook.com>
2021-03-06 23:13:07 -0500

    Miscelaneas1

--- MVC/Proyecto/registro/aplicacion/principal/models.py
+++ MVC/Proyecto/registro/aplicacion/principal/models.py
@@ -2,24 +2,25 @@ from django.db import models
 
 # Create your models here.
 
-class Persona(models.Model):
-    id = models.AutoField(primary_key = True)
-    nombres = models.CharField(max_length = 50)
-    apellidos = models.CharField(max_length = 50)
-    idtipodocumento
-    lugarresidencia
-    fechanacimiento = models.DateField()
-    email = models.CharField(max_length = 50)
-    telefono = models.IntegerField()
-    usuario = models.CharField(max_length = 30)
-    password = models.CharField(min_length = 10, max_length = 30)
-
 class TipoDocumento(models.Model):
     id = models.AutoField(primary_key = True)
-    nombre = models.CharField(unique = True)
+    nombre = models.CharField(unique = True, max_length = 30)
     descripcion = models.CharField(max_length = 100)
 
 class Ciudad(models.Model):
     id = models.AutoField(primary_key = True)
-    nombre = models.CharField(unique = True)
+    nombre = models.CharField(unique = True, max_length = 30)
     descripcion = models.CharField(max_length = 100)
+
+class Persona(models.Model):
+    id = models.AutoField(primary_key = True)
+    nombres = models.CharField(max_length = 50)
+    apellidos = models.CharField(max_length = 50)
+    idtipodocumento = models.ForeignKey(TipoDocumento, on_delete = models.CASCADE)
+    documento = models.IntegerField(unique = True)
+    lugarresidencia = models.ForeignKey(Ciudad, on_delete = models.CASCADE)
+    fechanacimiento = models.DateField()
+    email = models.EmailField(max_length = 50)
+    telefono = models.IntegerField()
+    usuario = models.CharField(max_length = 30)
+    password = models.CharField(max_length = 30)
